0b51e22db99dead764e01cbde4ce6daf9adcbc6d
Florin Motoc <dev@mflorin.com>
2024-11-04 14:56:53 +0200

    add laravel-stuff

---
+++ README.md
@@ -0,0 +1,10 @@
+# laravel-stuff
+
+## Installation
+
+Install the latest version with
+
+```bash
+composer require florinmotoc/laravel-stuff
+```
+
---
+++ src/LaravelStuffServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace FlorinMotoc\LaravelStuff;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelStuffServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
